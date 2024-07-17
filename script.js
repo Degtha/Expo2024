bb7564434cd8193eab1c1b3684c6c491b77388e0
primamahardika06 <primaodut06@gmail.com>
2024-07-17 11:13:47 +0800

    mengubah hyperlink menambah website projek & website cek khodam

--- script.js
+++ script.js
@@ -157,3 +157,55 @@ const buttonWA = document.querySelector(".floating-button")
 		buttonWA.addEventListener("click", function(){
   		document.location.href = "https://wa.me/+62895361262492?text=Halo%20kak%20saya%20mau%20daftar%20"
 		})
+
+
+
+// cekkhodam
+
+const khodams = [
+  ,'Kosong',
+  'Ban Tubles',
+  'Decul',
+  'Rx King',
+  'CBR Lecet',
+  'Kuda Jantan',
+  'Buah Naga',
+  'Singa Senyum',
+  'Naga Guling',
+  'Macan Tersenyum',
+  'Elang Ganteng',
+  'Kuda Bersemangat',
+  'Kelinci Gemas',
+  'Tikus Imut',
+  'Gajah Gembira',
+  'Kucing Menggemaskan',
+  'Anjing Setia',
+  'Katak Ceria',
+  'Kura-Kura Tangguh',
+];
+
+function cekKodam (){
+  const randomIndex = Math.floor(Math.random () * khodams.length)
+  const randomKodam = khodams[randomIndex] 
+  const nama = document.getElementById('nama').value.trim()
+
+  if (nama == '')
+  {
+      alert('isi nama anda')
+      document.getElementById('hasil').innerHTML = ``;
+      return
+  }
+
+  const hasil = document.getElementById('hasil')
+  document.getElementById('hasil').innerHTML = `<p>Khodam yang bersemayam dalam diri ${nama} adalah ${randomKodam}</p>`;
+
+  const masukkanNama = document.getElementById('nama')
+  masukkanNama.value = ''
+
+  
+}
+document.getElementById('kodam').addEventListener('submit', function (e) {
+  e.preventDefault(); 
+
+  cekKodam()
+})
